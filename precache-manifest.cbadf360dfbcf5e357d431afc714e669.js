c4996c2588281ba9d9648500cda5eaa7a81cbf53
Leonid <59738956+DoloreMentale@users.noreply.github.com>
2020-08-16 12:36:12 +0400

    Initial dist subtree commit

---
+++ precache-manifest.cbadf360dfbcf5e357d431afc714e669.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87df68014c8e996144c8",
+    "url": "/SimpleNotes/css/app.5f7bc933.css"
+  },
+  {
+    "revision": "ec44ab6aad254b943af9f69c7c2f541b",
+    "url": "/SimpleNotes/index.html"
+  },
+  {
+    "revision": "87df68014c8e996144c8",
+    "url": "/SimpleNotes/js/app.81cab496.js"
+  },
+  {
+    "revision": "3e09e87d03529ab83eb9",
+    "url": "/SimpleNotes/js/chunk-vendors.2fc18692.js"
+  },
+  {
+    "revision": "169cd3b6a45a05f121140d09c8f7ba32",
+    "url": "/SimpleNotes/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/SimpleNotes/robots.txt"
+  }
+]);
